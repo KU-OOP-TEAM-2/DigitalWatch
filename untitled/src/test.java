f4916c9cdb06183eccd074de3aa612fa25d326f5
JeonDY <pl04074@naver.com>
2020-05-08 14:47:02 +0900

    Update test.java

--- untitled/src/test.java
+++ untitled/src/test.java
@@ -3,7 +3,7 @@ public class test {
     public static void main(String[] args){
         int a=10;
         a = a+10;
-        System.out.println("ww");
+        System.out.println("www");
     }
 }
 
