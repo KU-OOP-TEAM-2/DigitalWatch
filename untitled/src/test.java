12a9ccb56b99a2a1304b2dbad37b9e8795da35dd
JeonDY <pl04074@naver.com>
2020-05-08 13:40:26 +0900

    Update test.java

--- untitled/src/test.java
+++ untitled/src/test.java
@@ -4,7 +4,7 @@ public class test {
         int a=10;
 
         a = a+10;
-        System.out.println("heldl00");
+        System.out.println("hedldl00");
 		System.out.println("helddel00");
 
     }
