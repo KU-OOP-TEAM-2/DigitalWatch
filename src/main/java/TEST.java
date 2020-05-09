cc5321f8c31603811c70ca9d5cf7f384373670ab
JeonDY <pl04074@naver.com>
2020-05-09 17:25:40 +0900

    delete

--- src/main/java/TEST.java
+++
@@ -1,5 +0,0 @@
-public class TEST {
-    public int return3(){return 3;}
-    public String returnName(){return "Name";}
-    public boolean returnTrue(){System.out.println("hello"); return true;}
-}
--- test1/src/main/java/test2.java
+++
@@ -1,8 +0,0 @@
-public class test2 {
-    public void main(String args[])
-    {
-        int a;
-        a = 0;
-        a = a+10;
-    }
-}
