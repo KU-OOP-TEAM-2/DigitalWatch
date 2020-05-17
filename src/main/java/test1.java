e246fee99abe8cce3a50a0700d10c65d3f394478
JeonDY <pl04074@naver.com>
2020-05-17 15:47:19 +0900

    Update test1.java

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,10 +2,9 @@ public class test1 {
     public void main(){
         int a;
         a = 3;
-
         a =add(a,5);
         a = multi(a,5);
-        System.out.println("result :" +a);
+        System.out.println("result :" + a);
     }
 
    int add(int a , int b){return a+b;}
