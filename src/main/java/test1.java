37fb6978110d7db23e6716b36501efce4d9fbc58
TTC1018 <39405316+TTC1018@users.noreply.github.com>
2020-05-28 10:22:27 +0900

    test commit

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,6 +1,7 @@
 public class test1 {
     public void main(){
         int a;
+        int c;
         a = 3;
         a =add(a,5);
         a = multi(a,5);
--- src/test/java/test1Test.java
+++ src/test/java/test1Test.java
@@ -5,6 +5,7 @@ public class test1Test extends TestCase {
 
     test1 t = new test1();
 
+
     public void testAdd() {
         int a=3;
         int b=5;
