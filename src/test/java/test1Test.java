b50643fd16695ddb1faf3757c8e41b1c971c7a04
JeonDY <pl04074@naver.com>
2020-06-17 18:18:04 +0900

    extraAdd

--- src/test/java/test1Test.java
+++ src/test/java/test1Test.java
@@ -16,7 +16,7 @@ public class test1Test extends TestCase {
         int a=3;
         int b = 5;
         assertEquals(15,t.multi(a,b));
-        assertEquals(20,t.multi(a+1,b));
+
     }
 
     public void tf() {
